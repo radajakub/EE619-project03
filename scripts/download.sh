fde99d0c9b4d0a6073c16ecf175c2a32cfad1c83
radajakub <dev.jakubrada@icloud.com>
2023-06-02 19:16:21 +0900

    Add scripts to synchronize remote machine

---
+++ scripts/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:results ~/Projects/EE619-project03/results
---
+++ scripts/upload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sshpass -p MFoRL2645 scp -r ~/Projects/EE619-project03/MINE 20236065@eelabg12.kaist.ac.kr:MINE
