144537a2f0c81e323b7da9965657f666bde3c461
radajakub <dev.jakubrada@icloud.com>
2023-06-02 19:04:35 +0900

    Add script + python versions

---
+++ scripts/connect.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sshpass -p MFoRL2645 ssh 20236065@eelabg12.kaist.ac.kr
+# sshpass -p MFoRL2645 ssh 20236065@eelabg13.kaist.ac.kr
+# sshpass -p MFoRL2645 ssh 20236065@eelabg14.kaist.ac.kr
