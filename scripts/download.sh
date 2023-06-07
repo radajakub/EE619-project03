fd9547d87cf995cfc5382960ca3cc20837ed6b59
radajakub <dev.jakubrada@icloud.com>
2023-06-07 21:36:13 +0900

    Remove unused scripts

--- scripts/download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:results ~/Projects/EE619-project03/results
--- scripts/upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sshpass -p MFoRL2645 scp -r ~/Projects/EE619-project03 20236065@eelabg12.kaist.ac.kr:~/
