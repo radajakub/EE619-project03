5fb14f6ef2cc6b37fcb2bcd00956181390791249
radajakub <dev.jakubrada@icloud.com>
2023-06-08 12:03:30 +0900

    Add download scripts

---
+++ scripts/download12.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/experiments ~/Projects/EE619-project03/outputs/newest_12
+sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/training.log ~/Projects/EE619-project03/outputs/newest_12/training.log
