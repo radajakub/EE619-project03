79d48fd0425fdca1adedab5f90e489c374bdba74
radajakub <dev.jakubrada@icloud.com>
2023-06-08 23:15:20 +0900

    Change .log file to .out file

--- scripts/download12.sh
+++ scripts/download12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/experiments ~/Projects/EE619-project03/outputs/newest_12
-sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/training.log ~/Projects/EE619-project03/outputs/newest_12/training.log
-sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model.pt ~/Projects/EE619-project03/outputs/newest_12/trained_model.log
+sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/training.out ~/Projects/EE619-project03/outputs/newest_12/training.out
+sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model.pt ~/Projects/EE619-project03/outputs/newest_12/trained_model.pt
 sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model_intermediate.pt ~/Projects/EE619-project03/outputs/newest_12/trained_model_intermediate.pt
