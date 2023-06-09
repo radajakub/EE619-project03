7eefb4de6a3e0d4e11bb1856dab36a5ef9aed6ce
radajakub <dev.jakubrada@icloud.com>
2023-06-09 18:37:44 +0900

    Improve download scripts

--- scripts/download13.sh
+++ scripts/download13.sh
@@ -3,4 +3,4 @@
 sshpass -p MFoRL2645 scp -r 20236065@eelabg13.kaist.ac.kr:EE619-project03/MINE/experiments ~/Projects/EE619-project03/outputs/newest_13
 sshpass -p MFoRL2645 scp -r 20236065@eelabg13.kaist.ac.kr:EE619-project03/MINE/ee619/training.out ~/Projects/EE619-project03/outputs/newest_13/training.out
 sshpass -p MFoRL2645 scp -r 20236065@eelabg13.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model.pt ~/Projects/EE619-project03/outputs/newest_13/trained_model.pt
-sshpass -p MFoRL2645 scp -r 20236065@eelabg13.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model_intermediate.pt ~/Projects/EE619-project03/outputs/newest_13/trained_model_intermediate.pt
+sshpass -p MFoRL2645 scp -r 20236065@eelabg13.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model_best.pt ~/Projects/EE619-project03/outputs/newest_13/trained_model_best.pt
--- scripts/download14.sh
+++ scripts/download14.sh
@@ -3,4 +3,4 @@
 sshpass -p MFoRL2645 scp -r 20236065@eelabg14.kaist.ac.kr:EE619-project03/MINE/experiments ~/Projects/EE619-project03/outputs/newest_14
 sshpass -p MFoRL2645 scp -r 20236065@eelabg14.kaist.ac.kr:EE619-project03/MINE/ee619/training.out ~/Projects/EE619-project03/outputs/newest_14/training.out
 sshpass -p MFoRL2645 scp -r 20236065@eelabg14.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model.pt ~/Projects/EE619-project03/outputs/newest_14/trained_model.pt
-sshpass -p MFoRL2645 scp -r 20236065@eelabg14.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model_intermediate.pt ~/Projects/EE619-project03/outputs/newest_14/trained_model_intermediate.pt
+sshpass -p MFoRL2645 scp -r 20236065@eelabg14.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model_best.pt ~/Projects/EE619-project03/outputs/newest_14/trained_model_best.pt
