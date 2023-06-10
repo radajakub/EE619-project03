233db41ec0fd039f2acaf36b1a8e4d942b77a65d
radajakub <dev.jakubrada@icloud.com>
2023-06-10 14:14:22 +0900

    Change download scripts

--- scripts/download12.sh
+++ scripts/download12.sh
@@ -3,4 +3,4 @@
 sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/experiments ~/Projects/EE619-project03/outputs/newest_12
 sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/training.out ~/Projects/EE619-project03/outputs/newest_12/training.out
 sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model.pt ~/Projects/EE619-project03/outputs/newest_12/trained_model.pt
-sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/trained_model_best.pt ~/Projects/EE619-project03/outputs/newest_12/trained_model_best.pt
+sshpass -p MFoRL2645 scp -r 20236065@eelabg12.kaist.ac.kr:EE619-project03/MINE/ee619/traind_model_best.pt ~/Projects/EE619-project03/outputs/newest_12/trained_model_best.pt
