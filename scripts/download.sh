4aa559e3800a0a2f62620f3a3a2304344d75ed93
radajakub <dev.jakubrada@icloud.com>
2023-06-11 18:09:04 +0900

    Enhanced download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,8 +4,14 @@ server_num=$1
 
 if (( $server_num > 11  )) && (( $server_num < 15 ))
 then
-    sshpass -p MFoRL2645 scp -r 20236065@eelabg${server_num}.kaist.ac.kr:EE619-project03/SAC/experiments ~/Projects/EE619-project03/outputs/newest_${server_num}
-    sshpass -p MFoRL2645 scp -r 20236065@eelabg${server_num}.kaist.ac.kr:EE619-project03/SAC/ee619/training.out ~/Projects/EE619-project03/outputs/newest_${server_num}/training.out
-    sshpass -p MFoRL2645 scp -r 20236065@eelabg${server_num}.kaist.ac.kr:EE619-project03/SAC/ee619/trained_model.pt ~/Projects/EE619-project03/outputs/newest_${server_num}/trained_model.pt
-    sshpass -p MFoRL2645 scp -r 20236065@eelabg${server_num}.kaist.ac.kr:EE619-project03/SAC/ee619/traind_model_best.pt ~/Projects/EE619-project03/outputs/newest_${server_num}/trained_model_best.pt
+    server_prefix=20236065@eelabg${server_num}.kaist.ac.kr:EE619-project03/SAC
+    local_prefix=./outputs/newest_${server_num}
+
+
+    sshpass -p MFoRL2645 scp -r ${server_prefix}/experiments ${local_prefix}/
+    sshpass -p MFoRL2645 scp -r ${server_prefix}/ee619/training.out ${local_prefix}/training.out
+
+    mkdir -p ${local_prefix}/intermediate_models/
+    sshpass -p MFoRL2645 scp -r ${server_prefix}/ee619/trained_model_\*.pt ${local_prefix}/intermediate_models
+    sshpass -p MFoRL2645 scp -r ${server_prefix}/ee619/trained_model.pt ${local_prefix}/trained_model.pt
 fi
